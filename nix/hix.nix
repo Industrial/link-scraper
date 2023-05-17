7b10d3125c6d762fbc7280d42656edbe31dbedfd
Tom Wieland <tom.wieland@gmail.com>
2023-05-17 18:24:58 +0200

    refactor(build): update ghc, disable hlint and lsp

--- nix/hix.nix
+++ nix/hix.nix
@@ -3,7 +3,7 @@
   
   # We use the latest supported and cached version
   # from github:input-output-hk/haskell.nix
-  compiler-nix-name = "ghc8107";
+  compiler-nix-name = "ghc961";
 
   # Enable for cross-platform build
   # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
@@ -15,6 +15,6 @@
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
-  shell.tools.hlint = "latest";
-  shell.tools.haskell-language-server = "latest";
+  # shell.tools.hlint = "latest";
+  # shell.tools.haskell-language-server = "latest";
 }
