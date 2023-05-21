2d16aa7bbc7cfe2615246b282f748fa69ea886b6
Tom Wieland <tom.wieland@gmail.com>
2023-05-21 16:59:32 +0200

    refactor(app): attempt at scraper

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,18 @@
 
 module Main (main) where
 
+import Control.Monad (forM_)
+import Data.ByteString.Char8 (putStrLn)
 import Data.List (nub)
-import Data.Text as T
-import System.Environment
-import Text.HTML.Scalpel
-import Text.HTML.Scalpel.Core (Selector, chroots)
+import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Text as T (Text)
+import Network.URI
+  ( URI (uriPath, uriScheme),
+    parseURI,
+    uriToString,
+  )
+import System.Environment (getArgs)
+import Text.HTML.Scalpel (Scraper, URL, attrs, scrapeURL)
 
 main :: IO ()
 main = getArgs >>= handleArgs
@@ -15,10 +22,35 @@ handleArgs :: [String] -> IO ()
 handleArgs [url] = listUrlsForSite url
 handleArgs _ = putStrLn "usage: list-all-images URL"
 
+createURI :: String -> String -> Maybe URI
+createURI page href = do
+  pageURI <- parseURI page
+  hrefURI <- parseURI href
+  let hrefURIPath = uriPath hrefURI
+  let updatedURI = pageURI {uriScheme = "https:", uriPath = hrefURIPath}
+  return updatedURI
+
+createURIs :: String -> [String] -> [URI]
+createURIs page = mapMaybe (createURI page)
+
+createLinksFromURIs :: [URI] -> [String]
+createLinksFromURIs = map (\a -> uriToString id a "")
+
+scrapeLinks :: String -> IO (Maybe [Text])
+scrapeLinks url = scrapeURL url $ attrs "href" "a"
+
 listUrlsForSite :: URL -> IO ()
 listUrlsForSite url = do
-  hrefs <- scrapeURL url (attrs "href" "a")
-  maybe printError printImages hrefs
+  visitPage url []
   where
-    printError = putStrLn "ERROR: Could not scrape the URL!"
-    printImages = mapM_ putStrLn
+    visitPage :: URL -> [URL] -> IO ()
+    visitPage page visited = do
+      hrefs <- scrapeLinks url
+      mapM_ putStrLn hrefs
+
+-- uris <- fmap (createURI page) hrefList
+-- let uris = createURIs page (nub hrefList)
+-- let links = createLinksFromURIs uris
+-- let newLinks = filter (`notElem` visited) links
+-- mapM_ putStrLn newLinks
+-- forM_ newLinks $ \newLink -> visitPage newLink (newLink : visited)
